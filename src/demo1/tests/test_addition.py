e4dccc59f6d2fadb933359f8438585f55404fe42
Sudeep Patel <93346617+SudeepPatel-0812@users.noreply.github.com>
2022-07-16 23:36:28 +0530

    initial commit

---
+++ src/demo1/tests/test_addition.py
@@ -0,0 +1,8 @@
+def integer_addition():
+    assert (1+1) == 2
+
+def float_addition():
+    assert 2.2 + 1.8 == 5
+
+integer_addition()
+float_addition()
---
+++ src/demo2/tests/test_subtraction.py
@@ -0,0 +1,8 @@
+def integer_subtraction():
+    assert (1-1) == 0
+
+def float_subtraction():
+    assert 2.2 - 1.8 == 1
+
+integer_subtraction()
+float_subtraction()
